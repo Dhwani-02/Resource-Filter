42cb025c9dc3da933f884d8bf5831e4e2734f23b
Dhwani-02 <dhwanizala25@gmail.com>
2024-06-07 17:49:29 +0530

    created plugin folder and that main file

---
+++ wp-content/plugins/resource_filter/resource_filter.php
@@ -0,0 +1,7 @@
+<?php/*
+ * Plugin Name:       Resource-Filter
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.1
+ * Author:            Dhwani Zala
+ * Domain Path:       /languages
+ */
